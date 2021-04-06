9f1742640a67ed4b0a77be59c35ad25d908e60e6
toplenboren <toplenboren@yandex.ru>
2021-04-07 04:45:48 +0500

    init: update github-workflow scripts

--- index.ts
+++ index.ts
@@ -6,8 +6,8 @@ const port = 3000
 
 const server = http.createServer((req: IncomingMessage, res: ServerResponse ) => {
 	res.statusCode = 200
-	res.setHeader("Content-Type", "text/plain")
-	const file: Buffer = fs.readFileSync('./index.html')
+	res.setHeader("Content-Type", "text/html")
+	const file: Buffer = fs.readFileSync("./index.html")
 	res.end(file.toString())
 })
 
