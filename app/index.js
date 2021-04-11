2d266a79b9d11477ceffc87184d94fd3620b3243
Lenikas <stuffer232@gmail.com>
2021-04-11 17:20:07 +0500

    some changes for test surge function

--- app/index.js
+++ app/index.js
@@ -1,9 +1,17 @@
 const http = require('http');
+const fs = require("fs");
 
 const hostname = '';
 const port = 8080;
+const surgeDomain = "http://messenger.ufru.surge.sh/"
 
 const server = http.createServer((req, res) => {
+  if(req.url == "/index.html") {
+    res.setHeader("Content-Type", "text/html");
+    res.writeHead(301, {Location: surgeDomain})
+    res.end();
+    return;
+  };
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('Hello World');
