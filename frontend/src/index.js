2240f10ab98946f2ecfa37c2dc3aa1119b57296f
Mikhail <toplenboren@gmail.com>
2021-04-29 23:55:35 +0500

    fix: on review

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import "./index.css"
 import CssBaseline from "@material-ui/core/CssBaseline"
 import { ThemeProvider } from "@material-ui/core/styles"
 import App from "./Components/App/App"
