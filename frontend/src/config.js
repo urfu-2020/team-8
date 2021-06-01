caa64089d9b732aa6ffe1ee5aafc96de23a89f41
Mikhail Gorbunov <toplenboren@gmail.com>
2021-06-01 20:17:40 +0500

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,7 @@
 export default function getConfig() {
 	return {
 		client_id: "f4fbae41dc2a65abe43e",
-		redirect_uri: "http://localhost:3000/login",
+		redirect_uri: "http://messenger.urfu.surge.sh/login",
 		proxy_url: "http://localhost:5000/api/authenticate"
 	}
 }
