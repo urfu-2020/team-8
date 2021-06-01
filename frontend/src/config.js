aa212dc5866818ea3e10c468906d3479222007ae
Mikhail <toplenboren@gmail.com>
2021-06-01 21:33:18 +0500

    init: set id

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,6 +1,6 @@
 export default function getConfig() {
 	return {
-		client_id: "f4fbae41dc2a65abe43e",
+		client_id: "61077180f9138ecc41ee",
 		redirect_uri: "http://messenger.urfu.surge.sh/login",
 		proxy_url: "https://messenger-ufru-course.herokuapp.com/api/authenticate"
 	}
