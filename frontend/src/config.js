805d336d1ee969e3f5617219fd0f9bf855746bfe
Svetlana1200 <sem.svetlana.m@mail.ru>
2021-06-05 22:57:52 +0500

    remove comment

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -5,11 +5,4 @@ export default function getConfig() {
 		proxy_url: "https://messenger-ufru-course.herokuapp.com/api/authenticate",
 		host: "https://messenger-ufru-course.herokuapp.com"
 	}
-
-	/*return {
-		client_id: "f4fbae41dc2a65abe43e",
-		redirect_uri: "http://localhost:3000/login",
-		proxy_url: "http://localhost:5000/api/authenticate",
-		host: "http://localhost:5000"
-	}*/
 }
